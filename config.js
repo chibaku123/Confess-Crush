660309ec7c07b9c3ea08a771e1fbb3219557560b
chibaku123 <102675021+chibaku123@users.noreply.github.com>
2022-03-30 21:02:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,11 +10,11 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bae lại yêu mình <3',
+    btnReply: 'Gửi cho bae <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà 🥰. Yêu bae nhiều nhiều 😘😘',
+    messDesc: 'Tối mai 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.messenger.com/t/100005991902107/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
